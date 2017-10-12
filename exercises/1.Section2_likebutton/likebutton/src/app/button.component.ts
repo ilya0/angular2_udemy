21539868df68c1a1bde796ef1994cb0ecdca6430
ilya0 <iosovets@gmail.com>
2017-10-12 10:29:01 -0700

    added exercises

---
+++ exercises/1.Section2_likebutton/likebutton/src/app/button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'button',
+    template:`
+    <input></input>
+    <br>
+    <button></button>`,
+    directive:''
+})
+
+export class ButtonComponent{
+
+}
