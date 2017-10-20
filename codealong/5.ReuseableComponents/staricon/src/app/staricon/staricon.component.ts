fac159a76e72c246763b9b98fa25f0abf2a9cdd0
ilya0 <iosovets@gmail.com>
2017-10-19 18:11:05 -0700

    added working code along

--- codealong/5.ReuseableComponents/staricon/src/app/staricon/staricon.component.ts
+++ codealong/5.ReuseableComponents/staricon/src/app/staricon/staricon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite',
@@ -7,11 +7,14 @@ import { Component, OnInit, Input, Output } from '@angular/core';
 })
 export class StariconComponent implements OnInit {
 @Input('isfavorite') isSelected : boolean; // is-favorite is a alias
-
+@Output() change = new EventEmitter();
   constructor() { }
   ngOnInit() {}
   onClick(){
     this.isSelected = !this.isSelected;
+    this.change.emit(); // publishes an event
   }
+
+  
 }
 
