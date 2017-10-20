2cc190a4566c728c6dc3edd59e36ae7b35efd9c0
ilya0 <iosovets@gmail.com>
2017-10-20 10:44:05 -0700

    added stuff

--- codealong/5.ReuseableComponents/staricon/src/app/app.component.ts
+++ codealong/5.ReuseableComponents/staricon/src/app/app.component.ts
@@ -1,19 +1,22 @@
+import { FavoriteChangedEventArgs } from './../../../../10.Routingandnavigation/hello-world/src/app/favorite/favorite.component';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
-
   post = {
     title:  "Title",
     isFavorite: true
   }
 
-  onFavoriteChanged(){
-    console.log("Favorite Changed");
+  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs ){
+    console.log("Favorite Changed", eventArgs);
   }
+
 }
--- codealong/5.ReuseableComponents/staricon/src/app/staricon/staricon.component.ts
+++ codealong/5.ReuseableComponents/staricon/src/app/staricon/staricon.component.ts
@@ -8,13 +8,21 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class StariconComponent implements OnInit {
 @Input('isfavorite') isSelected : boolean; // is-favorite is a alias
 @Output() change = new EventEmitter();
+
+
   constructor() { }
+
   ngOnInit() {}
+  
   onClick(){
     this.isSelected = !this.isSelected;
-    this.change.emit(); // publishes an event
+    this.change.emit({newValue: this.isSelected}); // publishes an event
   }
 
-  
+
+
 }
 
+export interface FavoriteChangedEventArgs{
+  newValue:boolean;
+}
