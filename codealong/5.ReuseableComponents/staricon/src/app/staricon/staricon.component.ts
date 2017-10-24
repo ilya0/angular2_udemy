07741215072373ee4f671616f38b64a5ccd3a056
ilya0 <iosovets@gmail.com>
2017-10-24 10:03:06 -0700

    morning

--- codealong/5.ReuseableComponents/staricon/src/app/staricon/staricon.component.ts
+++ codealong/5.ReuseableComponents/staricon/src/app/staricon/staricon.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './staricon.component.html',
-  styleUrls: ['./staricon.component.css']
+  styleUrls: ['./staricon.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+  // ViewEncapsulation.Emulated - emulates the shadow dom and adds aditional attributes to the css
+  
+  // ViewEncapsulation.Native - will only use the imported files in the css, and not the globablly imported files
+  // ViewEncapsulation.None - this will not encapsulate the styles and will be accessible to all
+
 })
-export class StariconComponent implements OnInit {
-@Input('isfavorite') isSelected : boolean; // is-favorite is a alias
-@Output() change = new EventEmitter();
+export class StariconComponent {
+@Input('isFavorite') isSelected : boolean; // is-favorite is a alias
+@Output('change') change = new EventEmitter(); //instantiate a new emitter
 
-
-  constructor() { }
-
-  ngOnInit() {}
   
   onClick(){
     this.isSelected = !this.isSelected;
-    this.change.emit({newValue: this.isSelected}); // publishes an event
+    this.change.emit({newValue: this.isSelected}); // publishes an event with the instanciated emitter
+    
   }
 
 
