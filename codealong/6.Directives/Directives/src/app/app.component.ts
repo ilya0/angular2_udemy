a5f513a93206c92e4ad833974a32c0fd03ad07f7
ilya0 <iosovets@gmail.com>
2017-10-24 19:03:47 -0700

    halfway through ngfor

--- codealong/6.Directives/Directives/src/app/app.component.ts
+++ codealong/6.Directives/Directives/src/app/app.component.ts
@@ -8,8 +8,13 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
   title = 'app';
-    
   courses:any = [1,2];
   viewMode = 'map';
+
+  array = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+  ];
   
 }
