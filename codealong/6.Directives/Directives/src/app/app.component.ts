e8c59cb45321feff4b9d57d79b0ccfb46ca9175a
ilya0 <iosovets@gmail.com>
2017-10-30 09:55:03 -0700

    morning

--- codealong/6.Directives/Directives/src/app/app.component.ts
+++ codealong/6.Directives/Directives/src/app/app.component.ts
@@ -12,10 +12,41 @@ export class AppComponent {
   viewMode = 'map';
   array:any;
 
-  tests = [
+  tests;
+  isSelected;
+onAdd(){
+  this.tests.push({id:4, name: 'course'});
+}
+
+onChange(course){
+  let index = this.tests.indexOf(course);
+  this.tests.splice(index,1);
+}
+
+
+loadCourses(){
+  this.tests = [
     {id: 1, name: 'course1'},
     {id: 2, name: 'course2'},
     {id: 3, name: 'course3'}
   ];
-  
 }
+
+
+///used if you have a large data set and have issues with performance
+
+trackCourse(index,course){
+  return course ? course.id : undefined;
+}
+onclick(){
+  this.isSelected =!this.isSelected
+}
+
+task = {
+  title: 'Reivew apps',
+  assignee:{
+    name: 'Jogn Smith'
+  }
+  }
+}
+
